688d0f6a2d202e7af23ba44b6769d81febfecf7b
kool2406 <lannhgch18393@fpt.edu.vn>
2020-06-22 19:55:04 +0700

    Update product.js

--- product.js
+++ product.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var publicDir = require("path").join(__dirname,'/public');
 router.use(express.static(publicDir));
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://kool2406:0961688824Kool@cluster0-y4ose.mongodb.net/test";
+var url = "mongodb+srv://kool2000:0961688824Kool@cluster0-y4ose.mongodb.net/test";
 
 
 
 router.get('/',async (req,res)=>{   
     let client = await MongoClient.connect(url);
-    let dbo = client.db("asmdb");
+    let dbo = client.db("Kool");
     let result = await dbo.collection("products").find({}).toArray();
     res.render('index',{products:result});
 });
@@ -18,41 +18,42 @@ router.get('/delete',async (req,res)=>{
     var ObjectID = require('mongodb').ObjectID;
     let condition = {"_id" : ObjectID(id)};
     let client= await MongoClient.connect(url);
-    let dbo = client.db("asmdb");
+    let dbo = client.db("Kool");
     await dbo.collection("products").deleteOne(condition);
     //
     // let results = await dbo.collection("products").find({}).toArray();
     // res.render('index',{products:results});
     res.redirect('/');
 });
+
 router.get('/insert',async(req,res)=>{
     res.render('insertProduct');
 });
 router.post('/doInsert',async(req,res)=>{
     let client= await MongoClient.connect(url);
-    let dbo = client.db("asmdb");
+    let dbo = client.db("Kool");
     let nameValue = req.body.txtName;
     let priceValue = req.body.txtPrice;
     let colorValue = req.body.txtColor;
-    if(colorValue!="Green"||colorValue!="Yellow"||colorValue!="Blue"){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('color should be one of 3 type: Green, Blue or Yellow!');
-    }
-    else{
-    let newProduct = {name : nameValue, price : priceValue,color:colorValue};
+   // if(colorValue!="Green"||colorValue!="Yellow"||colorValue!="Blue"){
+   //     res.writeHead(200, {'Content-Type': 'text/plain'});
+    //    res.end('color should be one of 3 type: Green, Blue or Yellow!');
+   // }
+  //  else{
+   let newProduct = {name : nameValue, price : priceValue,color:colorValue};
     await dbo.collection("products").insertOne(newProduct);
     console.log(newProduct);
     // let results = await dbo.collection("products").find({}).toArray();
     // res.render('index',{products:results});
     res.redirect('/');  
-    }
+   // }
 });
 router.get('/update',async(req,res)=>
 {
     let id = req.query.id;
     var ObjectID = require('mongodb').ObjectID; 
     let cliet = await MongoClient.connect(url);
-    let dbo = cliet.db('asmdb');
+    let dbo = cliet.db('Kool');
     let result = await dbo.collection("products").findOne({'_id' : ObjectID(id)});
     res.render('updateProduct',{products:result});
 })
@@ -67,7 +68,7 @@ router.post('/doUpdate', async(req,res)=>{
     let condition = {"_id" : ObjectID(id)};
     
     let client= await MongoClient.connect(url);
-    let dbo = client.db("asmdb");
+    let dbo = client.db("Kool");
     await dbo.collection("products").updateOne(condition,newValues);
     //
     // let results = await dbo.collection("products").find({}).toArray();
