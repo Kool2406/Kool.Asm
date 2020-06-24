1d89c420f0ce4d43e90cf8385de1d80e3f1f9125
kool2406 <lannhgch18393@fpt.edu.vn>
2020-06-24 09:12:24 +0700

    abc

--- product.js
+++ product.js
@@ -35,18 +35,14 @@ router.post('/doInsert',async(req,res)=>{
     let nameValue = req.body.txtName;
     let priceValue = req.body.txtPrice;
     let colorValue = req.body.txtColor;
-   // if(colorValue!="Green"||colorValue!="Yellow"||colorValue!="Blue"){
-   //     res.writeHead(200, {'Content-Type': 'text/plain'});
-    //    res.end('color should be one of 3 type: Green, Blue or Yellow!');
-   // }
-  //  else{
+
    let newProduct = {name : nameValue, price : priceValue,color:colorValue};
     await dbo.collection("products").insertOne(newProduct);
     console.log(newProduct);
     // let results = await dbo.collection("products").find({}).toArray();
     // res.render('index',{products:results});
     res.redirect('/');  
-   // }
+
 });
 router.get('/update',async(req,res)=>
 {
@@ -62,8 +58,11 @@ router.post('/doUpdate', async(req,res)=>{
     let nameValue = req.body.txtName;
     let priceValue = req.body.txtPrice;
     let colorValue = req.body.txtColor;
-    
-    let newValues ={$set : {name: nameValue,price:priceValue,color:colorValue}};
+ //   let datetimeValue = req.body.txtDateTime;
+    //let dateValues = Date.now();
+    let today = new Date();
+    let date =today.getHours()+':'+today.getMinutes()+':'+today.getSeconds()+'--'+ today.getDate()+'-'+(today.getMonth()+1)+'-'+ today.getFullYear();
+    let newValues ={$set : {name : nameValue,  price: priceValue, color : colorValue, datetime : date}};
     var ObjectID = require('mongodb').ObjectID;
     let condition = {"_id" : ObjectID(id)};
     
